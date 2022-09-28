ad23492e6632240b3079b9fbe603c084646203bd
Songhan ZHANG <shz@dlut.edu.cn>
2022-09-28 17:38:22 +0800

    Update TFEA.jl

--- src/TFEA.jl
+++ src/TFEA.jl
@@ -1,20 +1,7 @@
 module TFEA
 
 include("gen_Nodes_grid.jl")
-
-function cal_list_DOF(n_nodes::Int64, n_dir)
-    n_DOF = length(n_dir) * n_nodes
-    list_DOF = zeros(n_DOF,2)
-    counter = 0
-    for i_node = 1:n_nodes
-        for i_dir = 1:length(n_dir)
-            counter += 1
-            list_DOF[counter,1] = counter
-            list_DOF[counter,2] = i_node + 0.1*n_dir[i_dir]
-        end
-    end
-    return n_DOF, list_DOF
-end
+include("cal_list_DOF.jl")
 
 function cal_Kg(Nodes, Elements, Materials, Reals, list_DOF; Nodes_a = [])
 
