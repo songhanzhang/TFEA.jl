d5bdb4a23f227c48572c77b85c3a75edb542399f
Songhan ZHANG <shz@dlut.edu.cn>
2022-09-30 13:14:52 +0800

    Update cal_list_DOF.jl

--- src/cal_list_DOF.jl
+++ src/cal_list_DOF.jl
@@ -1,13 +1,13 @@
-function cal_list_DOF(n_nodes::Int64, n_dir)
+function cal_list_DOF(n_nodes::Int64, n_dir_list)
 
-    n_DOF = length(n_dir) * n_nodes
+    n_DOF = length(n_dir_list) * n_nodes
     list_DOF = zeros(n_DOF,2)
     counter = 0
     for i_node = 1:n_nodes
-        for i_dir = 1:length(n_dir)
+        for i_dir = 1:length(n_dir_list)
             counter += 1
             list_DOF[counter,1] = counter
-            list_DOF[counter,2] = i_node + 0.1*n_dir[i_dir]
+            list_DOF[counter,2] = i_node + 0.1*n_dir_list[i_dir]
         end
     end
 
