61c380a0f99454a0786ab348cc6c3cd160ecb941
Songhan ZHANG <shz@dlut.edu.cn>
2022-10-12 10:48:32 +0800

    Create cal_Te_2DBeam.jl

---
+++ src/cal_Te_2DBeam.jl
@@ -0,0 +1,17 @@
+function cal_Te_2DBeam(xi,yi,xj,yj)
+
+    Le = sqrt((xj-xi)^2+(yj-yi)^2)
+
+    ss = (yj-yi)/Le
+    cs = (xj-xi)/Le
+
+    Te = [ cs   ss    0    0    0    0
+          -ss   cs    0    0    0    0
+            0    0    1    0    0    0
+            0    0    0   cs   ss    0
+            0    0    0  -ss   cs    0
+            0    0    0    0    0    1 ]
+
+    return Te
+
+end
