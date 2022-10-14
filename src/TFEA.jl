696e0058abe216637ea4f8c251bbb0ca84e0a5eb
Songhan ZHANG <shz@dlut.edu.cn>
2022-10-14 10:03:53 +0800

    Update TFEA.jl

--- src/TFEA.jl
+++ src/TFEA.jl
@@ -2,8 +2,9 @@ module TFEA
 
 include("cal_list_DOF.jl")
 
-include("cal_Kg.jl")
 include("cal_Ke_bar_2DBar.jl")
+include("cal_Kg.jl")
+include("cal_Me_bar_2DBar.jl")
 include("cal_Mg.jl")
 include("cal_Te_2DBar.jl")
 
