52fcdf86fb77ecd5c33154ca99f485d4958b2ed7
Songhan ZHANG <shz@dlut.edu.cn>
2023-11-20 14:10:34 +0800

    Update TFEA.jl

--- src/TFEA.jl
+++ src/TFEA.jl
@@ -1,11 +1,21 @@
 module TFEA
 
-include("cal_list_DOF.jl")
+include("cal_Ke_2D_LGL_36.jl")
 include("cal_Ke_bar_2DBar.jl")
+include("cal_Ke_bar_2DEulerBeam.jl")
+include("cal_Ke_bar_3DEulerBeam.jl")
+include("cal_KeMe_QuadraticTriangular.jl")
 include("cal_Kg.jl")
+include("cal_KgMg.jl")
+include("cal_list_DOF.jl")
+include("cal_Me_2D_LGL_36.jl")
 include("cal_Me_bar_2DBar.jl")
+include("cal_Me_bar_2DEulerBeam.jl")
 include("cal_Mg.jl")
 include("cal_Te_2DBar.jl")
+include("cal_Te_2DBeam.jl")
+include("cal_Te_3DBeam.jl")
 include("gen_Nodes_grid.jl")
+
 
 end
