39d23e5ba6d4b45a492ac4adbfeafbd13dc57aad
Songhan ZHANG <shz@dlut.edu.cn>
2023-11-26 19:46:36 +0800

    Update plot_model_nodes.jl

--- src/plot_model_nodes.jl
+++ src/plot_model_nodes.jl
@@ -1,5 +1,5 @@
-function plot_model_nodes()
-    
+function plot_model_nodes(Nodes)
+
     scatter!(Nodes[:,2], Nodes[:,3], label = "",
              marker = :circle, markercolor = :dodgerblue, markersize = 3)
 
