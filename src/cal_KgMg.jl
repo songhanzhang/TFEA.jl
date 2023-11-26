efb3232c52e72acb4a62831a1eff52b05206f96a
Songhan ZHANG <shz@dlut.edu.cn>
2023-11-26 19:17:22 +0800

    Update cal_KgMg.jl

--- src/cal_KgMg.jl
+++ src/cal_KgMg.jl
@@ -160,6 +160,74 @@ function cal_KgMg(Nodes, Elements, Materials, Reals, list_DOF; Nodes_a = [])
             DOFs = [DOF_1;DOF_2;DOF_3;DOF_4;DOF_5;DOF_6;DOF_7;DOF_8;DOF_9;DOF_10;DOF_11;DOF_12]
             Kg[DOFs,DOFs] += Ke
             Mg[DOFs,DOFs] += Me
+        elseif Elements[i_e,2] == "2D_Acoustics_4N"
+            node_1 = Elements[i_e,5][1]
+            node_2 = Elements[i_e,5][2]
+            node_3 = Elements[i_e,5][3]
+            node_4 = Elements[i_e,5][4]
+            x1 = Nodes[node_1,2]
+            y1 = Nodes[node_1,3]
+            x2 = Nodes[node_2,2]
+            y2 = Nodes[node_2,3]
+            x3 = Nodes[node_3,2]
+            y3 = Nodes[node_3,3]
+            x4 = Nodes[node_4,2]
+            y4 = Nodes[node_4,3]
+            i_mat = Elements[i_e,3]
+            i_real = Elements[i_e,4]
+            c_air = Materials[i_mat,2][1]
+            t = Reals[i_real,2][1]
+            
+
+            Gauss = [ -1/(sqrt(3))  -1/(sqrt(3))  1
+                       1/(sqrt(3))  -1/(sqrt(3))  1
+                       1/(sqrt(3))   1/(sqrt(3))  1
+                      -1/(sqrt(3))   1/(sqrt(3))  1 ]
+            Ke = zeros(4,4)
+            for i_Gauss = 1:size(Gauss,1)
+                ξ = Gauss[i_Gauss,1]
+                η = Gauss[i_Gauss,2]
+                H = Gauss[i_Gauss,3]
+                N1 = 1/4*(1-ξ)*(1-η)
+                N2 = 1/4*(1+ξ)*(1-η)
+                N3 = 1/4*(1+ξ)*(1+η)
+                N4 = 1/4*(1-ξ)*(1+η)
+                dN1_dξ = -1/4*(1-η)
+                dN2_dξ =  1/4*(1-η)
+                dN3_dξ =  1/4*(1+η)
+                dN4_dξ = -1/4*(1+η)
+                dN1_dη = -1/4*(1-ξ)
+                dN2_dη = -1/4*(1+ξ)
+                dN3_dη =  1/4*(1+ξ)
+                dN4_dη =  1/4*(1-ξ)
+                dx_dξ = dN1_dξ*x1 + dN2_dξ*x2 + dN3_dξ*x3 + dN4_dξ*x4
+                dy_dξ = dN1_dξ*y1 + dN2_dξ*y2 + dN3_dξ*y3 + dN4_dξ*y4
+                dx_dη = dN1_dη*x1 + dN2_dη*x2 + dN3_dη*x3 + dN4_dη*x4
+                dy_dη = dN1_dη*y1 + dN2_dη*y2 + dN3_dη*y3 + dN4_dη*y4
+                J = [ dx_dξ  dy_dξ
+                      dx_dη  dy_dη ]
+                Temp = J\[ dN1_dξ; dN1_dη ]
+                dN1_dx = Temp[1]
+                dN1_dy = Temp[2]
+                Temp = J\[ dN2_dξ; dN2_dη ]
+                dN2_dx = Temp[1]
+                dN2_dy = Temp[2]
+                Temp = J\[ dN3_dξ; dN3_dη ]
+                dN3_dx = Temp[1]
+                dN3_dy = Temp[2]
+                Temp = J\[ dN4_dξ; dN4_dη ]
+                dN4_dx = Temp[1]
+                dN4_dy = Temp[2]
+                dN_dx = [ dN1_dx  dN2_dx  dN3_dx  dN4_dx ]
+                dN_dy = [ dN1_dy  dN2_dy  dN3_dy  dN4_dy ]
+                Ke += c_air^2*t*(transpose(dN_dx)*dN_dx + transpose(dN_dy)*dN_dy)*abs(det(J))*H
+            end
+            DOF_1 = findall(isequal(node_1 + 0.7),list_DOF[:,2])[1]
+            DOF_2 = findall(isequal(node_2 + 0.7),list_DOF[:,2])[1]
+            DOF_3 = findall(isequal(node_3 + 0.7),list_DOF[:,2])[1]
+            DOF_4 = findall(isequal(node_4 + 0.7),list_DOF[:,2])[1]
+            DOFs = [DOF_1;DOF_2;DOF_3;DOF_4]
+            Kg[DOFs,DOFs] += Ke
         end
     end
 
