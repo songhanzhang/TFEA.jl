98deb532fa212fc770e8f16703ac899688a349a4
Songhan ZHANG <shz@dlut.edu.cn>
2023-12-22 18:59:42 +0800

    Update gen_fun_CosHanning.jl

--- src/gen_fun_CosHanning.jl
+++ src/gen_fun_CosHanning.jl
@@ -3,6 +3,7 @@ function gen_fun_CosHanning(t_ax,fc,n_peaks,T_0)
     Ns = length(t_ax)
     ΔT = t_ax[2] - t_ax[1]
     n_steps = Int(floor(n_peaks/fc/ΔT))
+
     y = zeros(Ns)
     for ii = T_0 : T_0 + n_steps
         y[ii] = (1-cos(2*pi*fc*(ii-T_0)*ΔT/n_peaks))*sin(2*pi*fc*(ii-T_0)*ΔT)
