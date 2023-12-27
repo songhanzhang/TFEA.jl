b51295f3b9735af60b114fb9b779ec94e32ca380
Songhan ZHANG <shz@dlut.edu.cn>
2023-12-27 18:06:39 +0800

    Update main_wfem.jl

--- src/main_wfem.jl
+++ src/main_wfem.jl
@@ -14,6 +14,7 @@ Nodes = [
     4   0  -1   0
     5   0   1   0
 ]
+n_nodes = size(Nodes,1)
 
 Elements = [
     1  "2D_Euler_Beam"  1  1  (1,2)
@@ -21,7 +22,17 @@ Elements = [
     3  "2D_Euler_Beam"  1  1  (4,2)
     4  "2D_Euler_Beam"  1  1  (2,5)
 ]
+n_elements = size(Elements,1)
 
 Materials = [ 1  (2e11, 7850, 0.3) ]
 
-Reals = [ 1  (1) ]
+Reals = [ 1  (1, 1, 1, 1/64*pi*0.01^4) ]
+
+(n_DOF, list_DOF) = cal_list_DOF(n_nodes, [1,2,6])
+
+(Kg,Mg,Cg) = cal_KgMg(Nodes, Elements, Materials, Reals, list_DOF)
+
+P = zeros(15,15)
+for ii = 1:15
+    P[ii,ii] = 1
+end
