31bec75ff5ceea8393845811344db9c7a481dc93
Songhan ZHANG <shz@dlut.edu.cn>
2024-01-08 19:16:02 +0800

    Update cal_KeMe_Hexa.jl

--- src/cal_KeMe_Hexa.jl
+++ src/cal_KeMe_Hexa.jl
@@ -1,4 +1,4 @@
-function cal_KeMe_Hexa(x,y,z,E,ρ,ν)
+function cal_KeMe_Hexa(x,y,z,E,ρ,ν,pml_interface,model_boundary,eta_max)
     
     Nodes_xy = [ x y z ]
 
@@ -93,8 +93,35 @@ function cal_KeMe_Hexa(x,y,z,E,ρ,ν)
         NN = kron(N, [1 0 0; 0 1 0; 0 0 1])
         Me = Me + ρ*transpose(NN)*NN*abs(det(J))
         Ke = Ke + transpose(B)*D*B*abs(det(J))
+        if !isempty(pml_interface)
+            x_gauss = transpose(Nb)*x
+            y_gauss = transpose(Nb)*y
+            # eta_pml = cal_pml_eta(pml_interface, model_boundary, x_gauss, y_gauss, eta_max)
+            x_Lb = pml_interface[1]
+            x_Rb = pml_interface[2]
+            y_Bb = pml_interface[3]
+            y_Tb = pml_interface[4]
+
+            x_min = model_boundary[1]
+            x_max = model_boundary[2]
+            y_min = model_boundary[3]
+            y_max = model_boundary[4]
+
+            if x_gauss < x_Lb
+                eta_pml = eta_max*((x_Lb-x_gauss)/(x_Lb-x_min))^3
+            elseif x_gauss > x_Rb
+                eta_pml = eta_max*((x_gauss-x_Rb)/(x_max-x_Rb))^3
+            elseif y_gauss < y_Bb
+                eta_pml = eta_max*((y_Bb-y_gauss)/(y_Bb-y_min))^3
+            elseif y_gauss > y_Tb
+                eta_pml = eta_max*((y_gauss-y_Tb)/(y_max-y_Tb))^3
+            else
+                eta_pml = 0.0
+            end
+            Ce += Me * eta_pml
+        end
     end
 
-    return Me,Ce,Ke
+    return Ke,Me,Ce
 
 end
