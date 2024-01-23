fa32c4731895cb71b4a5aa2d36d2d70c4c34ade9
Songhan ZHANG <shz@dlut.edu.cn>
2024-01-24 02:22:02 +0800

    Update plot_model_elements.jl

--- src/plot_model_elements.jl
+++ src/plot_model_elements.jl
@@ -72,6 +72,20 @@ function plot_model_elements(Nodes, Elements)
             else
                 plot!(Shape(x_profile,y_profile), color = :pink, linewidth = 1)
             end
+        elseif Elements[i_e,2] == "2D_LGL_36n"
+            node_i = Elements[i_e,5][1]
+            node_j = Elements[i_e,5][6]
+            node_k = Elements[i_e,5][36]
+            node_m = Elements[i_e,5][31]
+            xi = Nodes[node_i,2]
+            yi = Nodes[node_i,3]
+            xj = Nodes[node_j,2]
+            yj = Nodes[node_j,3]
+            xk = Nodes[node_k,2]
+            yk = Nodes[node_k,3]
+            xm = Nodes[node_m,2]
+            ym = Nodes[node_m,3]
+            plot!(Shape([xi,xj,xk,xm], [yi,yj,yk,ym]), color = :gray80, w = 1)
         end
     end
 end
