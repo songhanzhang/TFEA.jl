fa8b253eda7ff2330702fb27eeb7714600d9b22d
Songhan ZHANG <shz@dlut.edu.cn>
2024-02-18 23:32:40 +0800

    Update judge_point_inside_triangle.jl

--- src/judge_point_inside_triangle.jl
+++ src/judge_point_inside_triangle.jl
@@ -1,4 +1,5 @@
 function judge_point_inside_triangle(x1,y1,x2,y2,x3,y3,xp,yp)
+
     S1 = 1/2 * ( x3*yp + xp*y2 + x2*y3 - x3*y2 - x2*yp - xp*y3 )
     S2 = 1/2 * ( x1*yp + xp*y3 + x3*y1 - x1*y3 - x3*yp - xp*y1 )
     S3 = 1/2 * ( x2*yp + xp*y1 + x1*y2 - x2*y1 - x1*yp - xp*y2 )
@@ -7,5 +8,7 @@ function judge_point_inside_triangle(x1,y1,x2,y2,x3,y3,xp,yp)
     else
         answer = false
     end
+    
     return answer
+
 end
