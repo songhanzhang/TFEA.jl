9416ca2b364b4faa6fd0d581f5da351b62f2eb94
Songhan ZHANG <shz@dlut.edu.cn>
2024-02-18 23:37:44 +0800

    Update cal_KeMe_Hexa.jl

--- src/cal_KeMe_Hexa.jl
+++ src/cal_KeMe_Hexa.jl
@@ -97,27 +97,28 @@ function cal_KeMe_Hexa(x,y,z,E,ρ,ν,pml_interface,model_boundary,eta_max)
             x_gauss = transpose(Nb)*x
             y_gauss = transpose(Nb)*y
             
-            x_Lb = pml_interface[1]
-            x_Rb = pml_interface[2]
-            y_Bb = pml_interface[3]
-            y_Tb = pml_interface[4]
+            # x_Lb = pml_interface[1]
+            # x_Rb = pml_interface[2]
+            # y_Bb = pml_interface[3]
+            # y_Tb = pml_interface[4]
 
-            x_min = model_boundary[1]
-            x_max = model_boundary[2]
-            y_min = model_boundary[3]
-            y_max = model_boundary[4]
+            # x_min = model_boundary[1]
+            # x_max = model_boundary[2]
+            # y_min = model_boundary[3]
+            # y_max = model_boundary[4]
 
-            if x_gauss < x_Lb
-                eta_pml = eta_max*((x_Lb-x_gauss)/(x_Lb-x_min))^3
-            elseif x_gauss > x_Rb
-                eta_pml = eta_max*((x_gauss-x_Rb)/(x_max-x_Rb))^3
-            elseif y_gauss < y_Bb
-                eta_pml = eta_max*((y_Bb-y_gauss)/(y_Bb-y_min))^3
-            elseif y_gauss > y_Tb
-                eta_pml = eta_max*((y_gauss-y_Tb)/(y_max-y_Tb))^3
-            else
-                eta_pml = 0.0
-            end
+            # if x_gauss < x_Lb
+            #     eta_pml = eta_max*((x_Lb-x_gauss)/(x_Lb-x_min))^3
+            # elseif x_gauss > x_Rb
+            #     eta_pml = eta_max*((x_gauss-x_Rb)/(x_max-x_Rb))^3
+            # elseif y_gauss < y_Bb
+            #     eta_pml = eta_max*((y_Bb-y_gauss)/(y_Bb-y_min))^3
+            # elseif y_gauss > y_Tb
+            #     eta_pml = eta_max*((y_gauss-y_Tb)/(y_max-y_Tb))^3
+            # else
+            #     eta_pml = 0.0
+            # end
+            eta_pml = cal_pml_eta(pml_interface,model_boundary,x_gauss,y_gauss,eta_max)
             Ce += Me * eta_pml
         end
     end
